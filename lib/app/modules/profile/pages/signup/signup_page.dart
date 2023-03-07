8a3a671f6c79f02daccb07890e86de4b356b2441
TiagoAlmeidaS <60373493+TiagoAlmeidaS@users.noreply.github.com>
2023-03-07 16:16:38 -0400

    Bug screen signup and signin (#9)
    Co-authored-by: 16687 <tiagoalmeida@bemol.com.br>

--- lib/app/modules/profile/pages/signup/signup_page.dart
+++ lib/app/modules/profile/pages/signup/signup_page.dart
@@ -28,98 +28,102 @@ class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SvgPicture.asset(logoSvg),
-          const SizedBox(
-            height: 20,
-          ),
-          TodoTextFormField(
-            label: "E-mail: ",
-            enabled: true,
-            hintText: "example@gmail.com",
-            rulesOnChange: controller.emailRulesOnChange,
-            keyboardType: TextInputType.emailAddress,
-            textCapitalization: TextCapitalization.none,
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          TodoTextFormField(
-            label: "Password: ",
-            enabled: true,
-            hintText: "123456",
-            rulesOnChange: controller.passwordRulesOnChange,
-            textCapitalization: TextCapitalization.none,
-          ),
-          TodoTextFormField(
-            label: "Confirm the password: ",
-            enabled: true,
-            hintText: "Ex: 123456",
-            rulesOnChange: controller.confirmPasswordRulesOnChange,
-            textCapitalization: TextCapitalization.none,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Observer(builder: (context) {
-            return TodoButton(
-              label: "Register",
-              onTap: () async {
-                controller.signup();
-                await controller.signupOutputObservable?.whenComplete(
-                  () => controller.signupOutputObservable?.value?.fold(
-                    (l) => TodoFlushBar(
-                      color: FlushBarColor.ERROR,
-                      message: l.message,
-                    ),
-                    (r) => Modular.to.pushNamed(
-                      CustomNavigationRouter.customNavigation.fullRoute,
-                    ),
-                  ),
-                );
-              },
-              state: controller.signupOutputObservable?.status ==
-                      FutureStatus.pending
-                  ? TodoButtonState.loadingFilledDark
-                  : controller.isValidRegister
-                      ? TodoButtonState.standardFilledDark
-                      : TodoButtonState.disabled,
-            );
-          }),
-          Text(
-            "Register with google: ",
-            style: Modular.get<ITodoTheme>().labelSignin,
-            textAlign: TextAlign.center,
-          ),
-          const TodoButton(
-              label: "Google", state: TodoButtonState.standardOutlined),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Do you have an account? ",
-                  style: Modular.get<ITodoTheme>().labelSignin,
-                ),
-                GestureDetector(
-                  child: Text(
-                    "Signin",
-                    style: Modular.get<ITodoTheme>().labelLink,
-                    textAlign: TextAlign.right,
-                  ),
-                  onTap: () {
-                    Modular.to.pushReplacementNamed(
-                      ProfileRouters.signin.fullRoute,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(logoSvg),
+              const SizedBox(
+                height: 20,
+              ),
+              TodoTextFormField(
+                label: "E-mail: ",
+                enabled: true,
+                hintText: "example@gmail.com",
+                rulesOnChange: controller.emailRulesOnChange,
+                keyboardType: TextInputType.emailAddress,
+                textCapitalization: TextCapitalization.none,
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              TodoTextFormField(
+                label: "Password: ",
+                enabled: true,
+                hintText: "123456",
+                rulesOnChange: controller.passwordRulesOnChange,
+                textCapitalization: TextCapitalization.none,
+              ),
+              TodoTextFormField(
+                label: "Confirm the password: ",
+                enabled: true,
+                hintText: "Ex: 123456",
+                rulesOnChange: controller.confirmPasswordRulesOnChange,
+                textCapitalization: TextCapitalization.none,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Observer(builder: (context) {
+                return TodoButton(
+                  label: "Register",
+                  onTap: () async {
+                    controller.signup();
+                    await controller.signupOutputObservable?.whenComplete(
+                          () => controller.signupOutputObservable?.value?.fold(
+                            (l) => TodoFlushBar(
+                          color: FlushBarColor.ERROR,
+                          message: l.message,
+                        ),
+                            (r) => Modular.to.pushNamed(
+                          CustomNavigationRouter.customNavigation.fullRoute,
+                        ),
+                      ),
                     );
                   },
+                  state: controller.signupOutputObservable?.status ==
+                      FutureStatus.pending
+                      ? TodoButtonState.loadingFilledDark
+                      : controller.isValidRegister
+                      ? TodoButtonState.standardFilledDark
+                      : TodoButtonState.disabled,
+                );
+              }),
+              Text(
+                "Register with google: ",
+                style: Modular.get<ITodoTheme>().labelSignin,
+                textAlign: TextAlign.center,
+              ),
+              const TodoButton(
+                  label: "Google", state: TodoButtonState.standardOutlined),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Do you have an account? ",
+                      style: Modular.get<ITodoTheme>().labelSignin,
+                    ),
+                    GestureDetector(
+                      child: Text(
+                        "Signin",
+                        style: Modular.get<ITodoTheme>().labelLink,
+                        textAlign: TextAlign.right,
+                      ),
+                      onTap: () {
+                        Modular.to.pushReplacementNamed(
+                          ProfileRouters.signin.fullRoute,
+                        );
+                      },
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          )
-        ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
